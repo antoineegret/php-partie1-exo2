c2e6adce390e3df64c001e365f4cd8591c0b0ee1
antoineegret <a.egret@laposte.net>
2019-12-05 14:31:58 +0100

    exercice 2

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exo1</title>
+</head>
+<body>
+<h1>exercie 2 & 7</h1>    
+Bonjour
+    <?php  
+    $firstname = "antoine ";
+    $lastname = "egret ";
+    $age = "27 ";
+    echo $firstname;
+     echo $lastname;
+     ?>
+     ,tu as <?php
+     echo $age; ?>
+     ans
+</body>
+</html>
